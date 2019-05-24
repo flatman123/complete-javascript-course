9289867e860daba3bb4038d6710aae9c85a67096
jeffrey mcintyre <Flatlandpyhon67@hotmail.com>
2019-05-24 13:08:03 -0700

    stuff

--- 6-budgety/starter/budgetController.js
+++ 6-budgety/starter/budgetController.js
@@ -2,24 +2,25 @@ var budgetController = (function() {
 	//we need a place to store our data. so we're going to use Function Contructors( Classes )
 	//Since it's a 'CLASS', it keeps track of all the incomes and expenses created.
 
-	var Expense = function(eipId, description, val) {
-		this.eipId = eipId;
+	var Expense = function(id, description, val) {
+		this.id = id;
 		this.description = description;
 		this.val = val;
 	};
 
-	var Income = function(eipId, description, val) {
-		this.eipId = eipId;
+	var Income = function(id, description, val) {
+		this.id = id;
 		this.description = description;
 		this.val = val;
 	};
 
 	var calculateTotal = function(type) {
 		var sum = 0;
-		data.allData[type].forEach(function(e,i,a) {
+		data.allData[type].forEach(function(e) {
 			sum += e.val;
 		});
 		data.allData[type].push(sum);
+		console.log(data.allData[type]);
 		};
 
 	var data = {
@@ -39,7 +40,7 @@ var budgetController = (function() {
 		var newItem, ID;
 		
 		if (data.allData[type].length > 0) {
-			ID = data.allData[type][data.allData[type].length - 1].eipId + 1;
+			ID = data.allData[type][data.allData[type].length -1].id + 1;
 		} else {
 			ID = 0;
 		}
@@ -49,10 +50,19 @@ var budgetController = (function() {
 		 	newItem = new Expense(ID, desc, val);
 		 }
 		 data.allData[type].push(newItem);
+		 
 		 return newItem;
+
+		 //
+		 //
+		 //FIX ISSUE WITH edID..NOT WORKING CORRECTLY.
+		 //
+		 //
+
 		},
-		calculateBudget : function(type) {
-			calculateTotal(type);			
+		calculateBudget : function() {
+			calculateTotal('exp');
+			calculateTotal('inc');			
 
 			// Calculate the budget
 			data.budget = data.totals.inc - data.totals.exp;
@@ -67,6 +77,9 @@ var budgetController = (function() {
 				totalExp: data.totals.exp,
 				percentage: data.percentage
 			}
+		},
+		testPublicAccess: function() {
+			console.log(data);
 		}
 	};
 })();
@@ -115,25 +128,25 @@ var uiController = (function() {
 			if (type === 'inc') {
 				element = domStrings.incomeContainer;
 
-			html = '<div class="item clearfix" id="income-%eipId%"> \
+			html = '<div class="item clearfix" id="income-%id%"> \
 			     	<div class="item__description">%description%</div> \
-			     	<div class="right clearfix"><div class="item__value">%val%</div>\
+			     	<div class="right clearfix"><div class="item__value">+ %val%</div>\
 			     	<div class="item__delete"><button class="item__delete--btn"> \
 			     	<i class="ion-ios-close-outline"></i></button></div></div></div>';
 
 			} else if (type === 'exp') {
 				element = domStrings.expenseContainer;
 
-			html =	'<div class="item clearfix" id="expense-%eipId%"> \
+			html =	'<div class="item clearfix" id="expense-%id%"> \
                     <div class="item__description">%description%</div> \
-                    <div class="right clearfix"><div class="item__value">%val%</div> \
+                    <div class="right clearfix"><div class="item__value">- %val%</div> \
                     <div class="item__percentage">21%</div><div class="item__delete"> \
                     <button class="item__delete--btn"><i class="ion-ios-close-outline"></i> \
                     </button></div></div></div>';
 			};
 
 			// Replace placeholder with object.value
-			newHtml = html.replace('%eipId%', obj.eipId);
+			newHtml = html.replace('%id%', obj.id);
 			newHtml = newHtml.replace('%description%', obj.description);
 			newHtml = newHtml.replace('%val%', obj.val);
 
@@ -149,7 +162,7 @@ var uiController = (function() {
 			fieldsArray = Array.prototype.slice.call(fields);
 
 			fieldsArray.forEach(function(e,i,a) {
-				e.val = '';
+				e.value = '';
 			});
 		},
 		displayResults: function() {
@@ -178,28 +191,40 @@ var  appController = (function(budgCtrl, uiCtrl) {
 			}
 		});
 	};
-		
+
+	var updateBudget = function() {
+		var budget;
+		//1. Calculate the Budget
+		budgCtrl.calculateBudget();
+
+		//2. Return the budget
+		budget = budgCtrl.getBudget();
+
+		//3. Display the budget on the ui
+		console.log(budget);
+	};
+
 	var crtlAddItem = function() {
 		var t,d,v, newItem, budget;
 		//1. Get the filed input data.
 		t = uiController.getInput().inType;
 		d = uiController.getInput().description;
 		v = uiController.getInput().value;
-		
 
 		if (d !== '' && !isNaN(v)) {
 
 			//2. Add New item to budget controler
 			newItem = budgCtrl.getData(t,d,v);
+			console.log(newItem);
 
 			//3. add item to user interface.
 			var output = uiCtrl.addListItem(newItem,t);
 			
-			//4. calculate the budget.
-			budgCtrl.calculateBudget(t);
-
+			//4. Clear Fields
+			uiCtrl.clearFields();
+			
 			//5. Return the budget.
-			budget = budgCtrl.getBudget();
+			updateBudget();
 
 		} else if (d !== '' && isNaN(v)) {
 			alert('Please Enter a Value amount!');
